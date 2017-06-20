0a749d9b26da3910e6c6a05353ffdaa11767cc0c
Denis Zakharenkov <zaharden@gmail.com>
2017-06-21 03:42:08 +0700

    Fix.

--- deploy/install-doxygen.sh
+++ deploy/install-doxygen.sh
@@ -4,6 +4,6 @@ set -e
 
 wget http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.13.linux.bin.tar.gz
 tar -xzf doxygen-1.8.13.linux.bin.tar.gz
-mv doxygen-1.8.13/bin/* /usr/bin/*
+mv ./doxygen-1.8.13/bin/* /usr/bin/
 #export PATH=$PATH:$(pwd)/doxygen-1.8.13/bin
 echo "Doxygen version: $(doxygen -v)"
